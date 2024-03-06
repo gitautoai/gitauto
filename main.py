c2b8baaf8c86ebdf53affd092fa2ae4ab4a99211
nikitamalinov <nikita@malinovsky.net>
2024-03-06 09:31:13 -0800

    adding sentry + testing in lambda

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 # Third-party imports
 from fastapi import FastAPI, HTTPException, Request
 from mangum import Mangum
+import sentry_sdk
+
 
 # Local imports
 from config import GITHUB_WEBHOOK_SECRET
@@ -14,6 +16,26 @@ from services.github.webhook_handler import handle_webhook_event
 app = FastAPI()
 handler = Mangum(app=app)
 
+from fastapi import FastAPI
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://b7ca4effebf7d7825b6464eade11734f@o4506827828101120.ingest.us.sentry.io/4506865231200256",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
+app = FastAPI()
+
+# Testing in Lambda
+@app.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
 
 @app.post(path="/webhook")
 async def handle_webhook(request: Request) -> dict[str, str]:
