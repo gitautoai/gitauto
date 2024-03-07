e310ddbce8e47b87eacd3af3eb48dd96f850bcc5
nikitamalinov <nikita@malinovsky.net>
2024-03-06 19:20:28 -0800

    adding to sentry

--- main.py
+++ main.py
@@ -17,16 +17,16 @@ app = FastAPI()
 handler = Mangum(app=app)
 
 from fastapi import FastAPI
+
 import sentry_sdk
+from sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration
 
 sentry_sdk.init(
     dsn="https://b7ca4effebf7d7825b6464eade11734f@o4506827828101120.ingest.us.sentry.io/4506865231200256",
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
+        integrations=[
+        AwsLambdaIntegration(),
+    ],
     traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
     profiles_sample_rate=1.0,
 )
 
