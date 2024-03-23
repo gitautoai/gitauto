a68233e7decde15b7a44763d219b8c27c46009e8
nikitamalinov <nikita@malinovsky.net>
2024-03-22 19:06:27 -0700

    .

--- tests/test_supabase_users.py
+++ tests/test_supabase_users.py
@@ -12,8 +12,23 @@ SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 
 
-def test_create_and_update_user_request() -> None:
+dummy = """
+# Dummy data in each environment
+installations: installation_id = 47287862, owner_name="nikita_dummy", owner_type="U", owner_id=1
+Users: User Id = 66699290, installation_id = 47287862
+
+installations: installation_id = 48567750, owner_name="lalager_dummy", owner_type="O", owner_id=4
+Users: User Id = 66699290, installation_id = 48567750
+
+installations: installation_id = 48332126, owner_name="gitautoai_dummy", owner_type="O", owner_id=3
+issues: installation_id = 48332126, unique_issue_id="U/gitautoai/nextjs-website#52"
+"""
+
+
+def test_create_and_update_user_request_works() -> None:
     supabase_manager = SupabaseManager(url=SUPABASE_URL, key=SUPABASE_SERVICE_ROLE_KEY)
+    print(SUPABASE_URL)
+    assert SUPABASE_URL == "https://atarljruiontapnenrsj.supabase.co"
     assert (
         supabase_manager.create_user_request(
             user_id=66699290,
@@ -31,7 +46,7 @@ def test_create_and_update_user_request() -> None:
     )
 
 
-# test_create_and_update_user_request()
+# test_create_and_update_user_request_works()
 
 
 def test_how_many_requests_left() -> None:
@@ -51,6 +66,7 @@ def test_how_many_requests_left() -> None:
     )
 
 
+# test_how_many_requests_left()
 def test_is_users_first_issue() -> None:
     supabase_manager = SupabaseManager(url=SUPABASE_URL, key=SUPABASE_SERVICE_ROLE_KEY)
     assert not supabase_manager.is_users_first_issue(
