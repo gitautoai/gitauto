cce4922d497ace4a3d97f08a2701961ff95ab26b
nikitamalinov <nikita@malinovsky.net>
2024-04-20 12:16:29 -0700

    Fixing linting errors

--- services/stripe/customer.py
+++ services/stripe/customer.py
@@ -1,5 +1,7 @@
-import stripe
+"""Stripe APIs for customer pricing management"""
+
 import logging
+import stripe
 from config import (
     STRIPE_API_KEY,
     STRIPE_FREE_TIER_PRICE_ID,
@@ -16,16 +18,17 @@ def subscribe_to_free_plan(
     owner_name: str,
     installation_id: int,
 ):
+    "Subscribe this customer to the free tier plan in stripe"
     stripe.Subscription.create(
         customer=customer_id,
         items=[{"price": STRIPE_FREE_TIER_PRICE_ID}],
         description="GitAuto Github App Installation Event",
         metadata={
-            "user_id": user_id,
+            "user_id": str(user_id),
             "user_name": user_name,
-            "owner_id": owner_id,
+            "owner_id": str(owner_id),
             "owner_name": owner_name,
-            "installation_id": installation_id,
+            "installation_id": str(installation_id),
         },
     )
 
@@ -33,13 +36,14 @@ def subscribe_to_free_plan(
 def create_stripe_customer(
     owner_name: str, owner_id: int, installation_id: int, user_id: int, user_name: str
 ) -> str:
+    """Create a customer with github metadata with Stripe"""
     customer = stripe.Customer.create(
         name=owner_name,
         metadata={
-            "owner_id": owner_id,
-            "installation_id": installation_id,
+            "owner_id": str(owner_id),
+            "installation_id": str(installation_id),
             "description": "GitAuto Github App Installation Event",
-            "user_id": user_id,
+            "user_id": str(user_id),
             "user_name": user_name,
         },
     )
@@ -47,11 +51,12 @@ def create_stripe_customer(
 
 
 def get_subscription(customer_id: str) -> stripe.ListObject[stripe.Subscription]:
+    """Get stripe subscription"""
     try:
         subscription = stripe.Subscription.list(customer=customer_id, status="active")
         return subscription
     except Exception as e:
-        logging.error(f"get_subscriptiont {e}")
+        logging.error("get_subscriptiont %s", e)
         raise
 
 
@@ -61,5 +66,5 @@ def get_request_count_from_product_id_metadata(product_id: str) -> int:
         price = stripe.Product.retrieve(product_id)
         return int(price["metadata"]["request_count"])
     except Exception as e:
-        logging.error(f"get_request_count_from_product_id_metadata {e}")
+        logging.error("get_request_count_from_product_id_metadata %s", e)
         raise
