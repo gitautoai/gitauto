f028588f7a8a68b795b73b9dc47bd01913cf3439
Hiroshi (Wes) Nishio <4620828+hiroshinishio@users.noreply.github.com>
2024-07-01 14:16:13 +0900

    Revert "Add sample Lambda function for periodic execution using EventBridge and codify EventBridge infrastructure with CloudFormation"

--- scheduler.py
+++
@@ -1,5 +0,0 @@
-# This is scheduled to run by AWS Lambda
-def schedule_handler(event, context) -> dict[str, int]:
-    print(f"Event: {event}")
-    print(f"Context: {context}")
-    return {"statusCode": 200}
