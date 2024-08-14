dc2f7e34fa8d1bf2dc3fe989cc7436a6b891bb0b
Hiroshi Nishio <hnishio0105@gmail.com>
2024-08-14 12:37:36 +0900

    Update OpenAI Model to 'gpt-4o-2024-08-06'

--- config.py
+++ config.py
@@ -41,14 +41,14 @@ GITHUB_WEBHOOK_SECRET: str = get_env_var(name="GH_WEBHOOK_SECRET")
 # OpenAI Credentials from environment variables
 OPENAI_API_KEY: str = get_env_var(name="OPENAI_API_KEY")
 OPENAI_ASSISTANT_NAME = "GitAuto: AI Coding Agent that generates GitHub pull requests from issues"
-OPENAI_FINAL_STATUSES: list[str] = ["cancelled", "completed", "expired", "failed"]
+OPENAI_FINAL_STATUSES = ["cancelled", "completed", "expired", "failed"]
 OPENAI_MAX_ARRAY_LENGTH = 32  # https://community.openai.com/t/assistant-threads-create-400-messages-array-too-long/754574/1
 OPENAI_MAX_STRING_LENGTH = 256000  # https://community.openai.com/t/assistant-threads-create-400-messages-array-too-long/754574/5
 OPENAI_MAX_CONTEXT_TOKENS = 128000  # https://gitauto-ai.sentry.io/issues/5582421515/events/9a09416e714c4a66bf1bd86916702be2/?project=4506865231200256&referrer=issue_details.related_trace_issue
 OPENAI_MAX_RETRIES = 3
 OPENAI_MAX_TOOL_OUTPUTS_SIZE = 512 * 1024  # in bytes
 OPENAI_MAX_TOKENS = 4096
-OPENAI_MODEL_ID = "gpt-4o"
+OPENAI_MODEL_ID = "gpt-4o-2024-08-06"
 OPENAI_ORG_ID: str = get_env_var(name="OPENAI_ORG_ID")
 OPENAI_TEMPERATURE = 0.0
 
