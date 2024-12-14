7a4af9b91308a55c25ef78c639423950dab98d70
Hiroshi Nishio <hnishio0105@gmail.com>
2024-12-14 14:36:12 +0900

    Stop skipping check run review in dev env

--- services/check_run_handler.py
+++ services/check_run_handler.py
@@ -3,6 +3,7 @@ import json
 from config import (
     EMAIL_LINK,
     GITHUB_APP_USER_NAME,
+    IS_PRD,
     STRIPE_PRODUCT_ID_FREE,
     SUPABASE_URL,
     SUPABASE_SERVICE_ROLE_KEY,
@@ -112,7 +113,7 @@ def handle_check_run(payload: CheckRunCompletedPayload) -> None:
 
     # Return here if product_id is not found or is in free tier
     product_id: str | None = get_stripe_product_id(customer_id=stripe_customer_id)
-    if product_id is None or product_id == STRIPE_PRODUCT_ID_FREE:
+    if product_id is None or product_id == STRIPE_PRODUCT_ID_FREE and IS_PRD:
         msg = f"Skipping because product_id is not found or is in free tier. product_id: '{product_id}'"
         print(colorize(text=msg, color="yellow"))
         return
