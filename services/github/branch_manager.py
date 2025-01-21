3150e6b3e808671ae696208cf1f65fc6ae37b1b4
Hiroshi Nishio <hnishio0105@gmail.com>
2025-01-20 17:22:17 -0800

    Fix an error: target branch is not the default branch like main

--- services/github/branch_manager.py
+++ services/github/branch_manager.py
@@ -6,12 +6,20 @@ from utils.handle_exceptions import handle_exceptions
 
 @handle_exceptions(default_return_value="main", raise_on_error=False)
 def get_default_branch(owner: str, repo: str, token: str):
-    """https://docs.github.com/en/rest/branches/branches?apiVersion=2022-11-28#list-branches"""
-    url = f"{GITHUB_API_URL}/repos/{owner}/{repo}/branches"
+    # Get the default branch name first
+    # https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#get-a-repository
+    url = f"{GITHUB_API_URL}/repos/{owner}/{repo}"
     headers = create_headers(token=token)
     response = requests.get(url=url, headers=headers, timeout=TIMEOUT)
     response.raise_for_status()
     data = response.json()
-    default_branch_name: str = data[0]["name"]
-    latest_commit_sha: str = data[0]["commit"]["sha"]
+    default_branch_name: str = data["default_branch"]
+
+    # Get the latest commit SHA for the default branch
+    # https://docs.github.com/en/rest/branches/branches?apiVersion=2022-11-28#get-a-branch
+    branch_url = f"{url}/branches/{default_branch_name}"
+    branch_response = requests.get(url=branch_url, headers=headers, timeout=TIMEOUT)
+    branch_response.raise_for_status()
+    branch_data = branch_response.json()
+    latest_commit_sha: str = branch_data["commit"]["sha"]
     return default_branch_name, latest_commit_sha
