854af10f7f64dc569097a631a96e101fe986e436
Hiroshi Nishio <hnishio0105@gmail.com>
2025-01-28 21:40:23 -0800

    .

--- tests/constants.py
+++ tests/constants.py
@@ -3,5 +3,5 @@ from services.github.github_manager import get_installation_access_token
 
 OWNER = "gitautoai"
 REPO = "gitauto"
-INSTALLATION_ID = "53743403"
+INSTALLATION_ID = 53743403
 TOKEN = get_installation_access_token(installation_id=INSTALLATION_ID)
