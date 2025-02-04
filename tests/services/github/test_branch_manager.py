cab70d18087414bc5e70ea019b8bc1a78fb9a9b6
Hiroshi Nishio <hnishio0105@gmail.com>
2025-02-04 15:51:19 -0800

    Refactor how we get github app installation token for testing

--- tests/services/github/test_branch_manager.py
+++ tests/services/github/test_branch_manager.py
@@ -1,14 +1,10 @@
 from services.github.branch_manager import get_default_branch
-from services.github.github_manager import get_installation_access_token
-from tests.constants import INSTALLATION_ID, OWNER, REPO
+from tests.constants import OWNER, REPO, TOKEN
 
 
 def test_get_default_branch() -> None:
-    # Setup
-    token = get_installation_access_token(installation_id=INSTALLATION_ID)
-
     # Exercise
-    default_branch, commit_sha = get_default_branch(OWNER, REPO, token)
+    default_branch, commit_sha = get_default_branch(OWNER, REPO, TOKEN)
 
     # Verify
     assert default_branch == "main"
