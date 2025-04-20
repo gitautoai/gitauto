73b2f3d871b2b6b2e52cb54ed25a9f0ff2abcba7
Hiroshi Nishio <hnishio0105@gmail.com>
2025-04-20 23:53:04 +0900

    Add a new function calling: replace_remote_file

---
+++ services/openai/functions/properties.py
@@ -0,0 +1,4 @@
+FILE_PATH: dict[str, str] = {
+    "type": "string",
+    "description": "The full path to the file within the repository. For example, 'src/openai/__init__.py'. NEVER EVER be the same as the file_path in previous function calls.",
+}
