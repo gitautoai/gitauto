93eba24c3acfdea815aab8fabe8aa744595edb58
Hiroshi (Wes) Nishio <hnishio0105@gmail.com>
2025-04-23 20:31:53 +0900

    Update test_graphql_client.py

--- services/github/test_graphql_client.py
+++ services/github/test_graphql_client.py
@@ -3,19 +3,19 @@ from config import GITHUB_API_URL
 from gql.transport.requests import RequestsHTTPTransport
 
 def test_get_graphql_client(monkeypatch):
-    monkeypatch.setattr("services.github.graphql_client.GITHUB_API_URL", GITHUB_API_URL)
+    monkeypatch.setattr("services.github.graphql_client.GITHUB_API_URL", "https://api.github.com")
     token = "dummy_token"
     client = get_graphql_client(token)
     transport = client.transport
     assert isinstance(transport, RequestsHTTPTransport)
-    assert transport.url == f"{GITHUB_API_URL}/graphql"
+    assert transport.url == "https://api.github.com/graphql"
     assert transport.headers["Authorization"] == "Bearer dummy_token"
     assert transport.verify is True
     assert transport.retries == 3
     assert client.fetch_schema_from_transport is True
 
 def test_empty_token(monkeypatch):
-    monkeypatch.setattr("services.github.graphql_client.GITHUB_API_URL", GITHUB_API_URL)
+    monkeypatch.setattr("services.github.graphql_client.GITHUB_API_URL", "https://api.github.com")
     token = ""
     client = get_graphql_client(token)
     transport = client.transport
