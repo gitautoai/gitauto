fec393a849dc505494da3f017dbcdee5781a0712
Hiroshi (Wes) Nishio <hnishio0105@gmail.com>
2025-05-21 15:08:58 +0900

    Delete services/github/comments/test_create_comment_new.py

--- services/github/comments/test_create_comment_new.py
+++
@@ -1,3 +0,0 @@
-    assert result is None  # The handle_exceptions decorator should return None on error
-    assert "headers" in mock_post.call_args[1]
-    assert mock_post.call_args[1]["json"] == {"body": "Test comment"}
