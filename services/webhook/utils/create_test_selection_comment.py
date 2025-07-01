9c99772028686092c353a4317d207655a1e33ace
Hiroshi Nishio <hnishio0105@gmail.com>
2025-07-01 15:05:27 -0700

    .

--- services/webhook/utils/create_test_selection_comment.py
+++ services/webhook/utils/create_test_selection_comment.py
@@ -1,4 +1,5 @@
 from typing import TypedDict
+from config import PRODUCT_NAME
 from constants.messages import SETTINGS_LINKS
 from services.github.pulls.get_pull_request_files import Status
 from utils.text.comment_identifiers import TEST_SELECTION_COMMENT_IDENTIFIER
@@ -31,7 +32,9 @@ def create_test_selection_comment(checklist: list[FileChecklistItem], branch_nam
             "",
             "---",
             "",
-            "- [ ] Manage Tests",
+            "- [ ] Yes, manage tests",
+            "",
+            f"Click the checkbox and {PRODUCT_NAME} will add/update/remove tests for the selected files to this PR.",
             "",
         ]
     )
--- utils/text/reset_command.py
+++ utils/text/reset_command.py
@@ -1,9 +1,8 @@
+from config import PRODUCT_NAME
+
+
 def create_reset_command_message(branch_name: str) -> str:
-    return f"""
-
----
-
-If GitAuto's commits are not satisfactory, you can reset to your original state from your local branch:
+    return f"""If {PRODUCT_NAME}'s commits are not satisfactory, you can reset to your original state from your local branch:
 
 ```bash
 git checkout {branch_name}
