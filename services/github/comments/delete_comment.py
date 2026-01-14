f0f6aa2d956e2a0c9cd980909f207621a45dca8e
Hiroshi Nishio <hnishio0105@gmail.com>
2026-01-14 18:09:48 +0900

    Handle 404 silently in delete_comment to reduce Sentry noise

--- services/github/comments/delete_comment.py
+++ services/github/comments/delete_comment.py
@@ -1,4 +1,5 @@
-from requests import delete
+from requests import HTTPError, delete
+
 from config import GITHUB_API_URL, TIMEOUT
 from services.github.utils.create_headers import create_headers
 from utils.error.handle_exceptions import handle_exceptions
@@ -10,4 +11,11 @@ def delete_comment(*, owner: str, repo: str, token: str, comment_id: int):
     url = f"{GITHUB_API_URL}/repos/{owner}/{repo}/issues/comments/{comment_id}"
     headers: dict[str, str] = create_headers(token=token)
     response = delete(url=url, headers=headers, timeout=TIMEOUT)
-    response.raise_for_status()
+
+    try:
+        response.raise_for_status()
+    except HTTPError:
+        if response.status_code == 404:
+            return None
+        raise
+    return None
