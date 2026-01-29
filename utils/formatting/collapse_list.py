d3ef2f2ef9b69c3224dad609d8dd5e75500ad069
Hiroshi Nishio <hnishio0105@gmail.com>
2026-01-29 12:49:06 +0900

    Simplify collapse_list type hint to list[str]

--- utils/formatting/collapse_list.py
+++ utils/formatting/collapse_list.py
@@ -1,10 +1,8 @@
-from typing import Sequence
-
 from utils.error.handle_exceptions import handle_exceptions
 
 
 @handle_exceptions(default_return_value="", raise_on_error=False)
-def collapse_list(items: Sequence[str], max_items: int = 6):
+def collapse_list(items: list[str], max_items: int = 6):
     """Format a list for display, collapsing the middle if too long.
 
     Shows first half and last half of items, with count of omitted items in middle.
